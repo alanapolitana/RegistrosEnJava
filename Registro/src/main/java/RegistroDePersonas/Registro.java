8d3bdb98bd9140635cf73350dff9f69837e61150
Marco <marcovirinni@gmail.com>
2023-11-19 10:07:34 -0300

    comenzando

---
+++ Registro/src/main/java/RegistroDePersonas/Registro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package RegistroDePersonas;
+
+/**
+ *
+ * @author virin
+ */
+public class Registro {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
