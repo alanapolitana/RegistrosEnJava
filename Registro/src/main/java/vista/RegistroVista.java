e1d4dacf4a537092a33a96fd2d29887577d5bc20
Marco <marcovirinni@gmail.com>
2023-11-30 20:08:10 -0300

    validaciones en tiempo y dato

--- Registro/src/main/java/vista/RegistroVista.java
+++ Registro/src/main/java/vista/RegistroVista.java
@@ -1,11 +1,14 @@
-// En el paquete vista
 package vista;
 
 import controlador.RegistroController;
 import modelo.Persona;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class RegistroVista {
     private RegistroController controlador;
@@ -42,23 +45,65 @@ public class RegistroVista {
     }
 
     private void ingresarPersona() {
-        System.out.println("Ingrese los datos de la persona:");
-        System.out.print("Nombre: ");
-        String nombre = scanner.next();
-        System.out.print("Apellido: ");
-        String apellido = scanner.next();
-        System.out.print("DNI: ");
-        String dni = scanner.next();
-        System.out.print("Fecha de Nacimiento (dd/mm/yyyy): ");
-        String fechaNacimiento = scanner.next();
+        String nombre;
+        String apellido;
+        String dni;
+        String fechaNacimiento;
+
+        // Validación del nombre
+        do {
+            System.out.print("Ingrese el nombre: ");
+            nombre = scanner.next();
+            if (nombre.isBlank() || !esLetras(nombre)) {
+                System.out.println("Error: El nombre debe contener solo letras. Inténtelo de nuevo.");
+            }
+        } while (nombre.isBlank() || !esLetras(nombre));
+
+        // Validación del apellido
+        do {
+            System.out.print("Ingrese el apellido: ");
+            apellido = scanner.next();
+            if (apellido.isBlank() || !esLetras(apellido)) {
+                System.out.println("Error: El apellido debe contener solo letras. Inténtelo de nuevo.");
+            }
+        } while (apellido.isBlank() || !esLetras(apellido));
+
+        // Validación del DNI
+        do {
+            System.out.print("Ingrese el DNI (entre 8 y 12 dígitos): ");
+            dni = scanner.next();
+            if (!dni.matches("\\d{8,12}")) {
+                System.out.println("Error: El DNI debe contener entre 8 y 12 dígitos numéricos. Inténtelo de nuevo.");
+            }
+        } while (!dni.matches("\\d{8,12}"));
+
+        // Validación de la fecha de nacimiento
+        do {
+            System.out.print("Ingrese la Fecha de Nacimiento (dd/mm/yyyy): ");
+            fechaNacimiento = scanner.next();
+            if (!validarFecha(fechaNacimiento)) {
+                System.out.println("Error: La fecha de nacimiento debe tener el formato dd/mm/yyyy. Inténtelo de nuevo.");
+            }
+        } while (!validarFecha(fechaNacimiento));
 
         Persona persona = new Persona(nombre, apellido, dni, fechaNacimiento);
+        controlador.agregarPersona(persona);
+        System.out.println("Persona registrada con éxito.");
+    }
 
-        if (persona.validarDatos()) {
-            controlador.agregarPersona(persona);
-            System.out.println("Persona registrada con éxito.");
-        } else {
-            System.out.println("Error: Datos no válidos. Inténtelo de nuevo.");
+    private boolean esLetras(String texto) {
+        return Pattern.matches("[a-zA-Z]+", texto);
+    }
+
+    private boolean validarFecha(String fecha) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        dateFormat.setLenient(false);
+
+        try {
+            Date parsedDate = dateFormat.parse(fecha);
+            return true;
+        } catch (ParseException e) {
+            return false;
         }
     }
 
